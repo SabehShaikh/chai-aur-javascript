cee867e5f1db4d0c35623ce0b716b3b4355e7729
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-11-23 11:12:29 +0500

    revised DataTypes and Conversion Operations.

---
+++ Class05-DataTypes/app.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs , not browser
+
+console.log(3 + 3);
+console.log("sabeh");
+
+let name = "sabeh";
+let age = 19;
+let isLoggedIn = false;
+
+// Primary Data Types -->
+// number => 2 to power 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined => abhi value assign nh hui
+// symbol => to find unique
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
