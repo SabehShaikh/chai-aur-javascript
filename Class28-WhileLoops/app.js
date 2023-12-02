5413b14eae61ca09c42826c91c9de0509c72679a
SabehShaikh <121764329+SabehShaikh@users.noreply.github.com>
2023-12-02 23:21:40 +0500

    Add While Loops.

---
+++ Class28-WhileLoops/app.js
@@ -0,0 +1,23 @@
+// while loop
+
+let i = 0;
+while (i <= 10) {
+  console.log(`value of i is ${i}`);
+  i++;
+}
+
+let myArray = ["flash", "spiderman", "ironman"];
+let arr = 0;
+
+while (arr < myArray.length) {
+  console.log(`value is ${myArray[arr]}`);
+  arr++;
+}
+
+// Do while Loop
+let score = 11;
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+} while (score <= 10);
